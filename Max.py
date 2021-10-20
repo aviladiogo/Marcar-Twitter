d386e78eedcbe8afa3abbe9c42abea5c1d6e8349
aviladiogo <aviladiogo30@gmail.com>
2021-10-20 16:27:21 -0300

    1.1

--- Max.py
+++ Max.py
@@ -1,20 +1,20 @@
-from keyboard import press_and_release, write
-from time import sleep, localtime
+import schedule
 import webbrowser
+from keyboard import press_and_release, write
+from time import sleep
 from pynput.mouse import Button, Controller
 
 mouse = Controller()
-
 
 def marca():
     press_and_release('shift+2') # @
     write('victorialuquett')
     press_and_release('enter')
-    sleep(1)
+    sleep(0.5)
     press_and_release('shift+2') # @
     write('Quequeh0uve')
     press_and_release('enter')
-    sleep(1)
+    sleep(0.5)
     press_and_release('shift+2') # @
     write('juuldecereja')
     press_and_release('enter')
@@ -25,27 +25,23 @@ def fechar():
     mouse.position = (1514, 11) # envia o cursor para o botão de fechar o chrome
     mouse.click(Button.left, 1)
 
-while True:
-    hora = localtime()
-    if(hora.tm_hour != 14): # caso não seja 14 horas  
-        print("esperando proxima hora")
-        sleep(3600)
-    elif(hora.tm_hour == 14 and hora.tm_min == 30): # caso seja a hora exata do novo post
-        webbrowser.open('https://twitter.com/vomaxroupa', new=2) # abre a pagina do twitter no chrome
-        sleep(8)
-        mouse.position = (739, 803) # Abre a Publicação mais recente
-        sleep(0.1)
-        mouse.click(Button.left, 1)
-        sleep(2)
-        press_and_release('r') # Aperta R para responder o tweet
-        sleep(2)
-        marca()
-        sleep(2)
-        envia()
-        sleep(2)
-        fechar()
-        sleep(60)
-    else:
-        print("esperando minutagem") #caso seja 14 horas porém não seja 14:30
-        print(hora.tm_min)
-        sleep(60)
+def VôMax():
+    webbrowser.open('https://twitter.com/vomaxroupa', new=2) # abre a pagina do twitter no chrome
+    sleep(5)
+    mouse.position = (739, 803) # Abre a Publicação mais recente
+    sleep(0.1)
+    mouse.click(Button.left, 1)
+    sleep(0.1)
+    press_and_release('r') # Aperta R para responder o tweet
+    sleep(0.5)
+    marca()
+    sleep(0.5)
+    envia()
+    sleep(0.1)
+    fechar()   
+
+schedule.every().day.at("14:30").do(VôMax) # Função da biblioteca Schedule, ira todo dia as 14:30 realizar a função 
+
+while 1:
+    schedule.run_pending()
+    sleep(1)
